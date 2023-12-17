563c2d90191202f5593b380d74d11ef1e2c96ea6
ZeroDajin <126646596+ZeroDajin@users.noreply.github.com>
2023-12-17 23:13:18 +0700

    add discard old data

--- src/main/java/ClientHandler/WebcamDisplayWithUDP.java
+++ src/main/java/ClientHandler/WebcamDisplayWithUDP.java
@@ -13,6 +13,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import javax.imageio.ImageIO;
 /**
@@ -23,6 +24,8 @@ public class WebcamDisplayWithUDP {
     private DatagramSocket socket;
     private InetAddress destinationIP;
     private int destinationPort;
+    private byte[][] circularBuffer = new byte[10][65537];
+    private int nextIndex = 0;
     public WebcamDisplayWithUDP(String destinationIP, int destinationPort) {
         this.destinationPort = destinationPort;
         try {
@@ -32,41 +35,58 @@ public class WebcamDisplayWithUDP {
             e.printStackTrace();
         }
     }
-        public void sendBufferedImageOverUDP(BufferedImage img) {
-    try {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(img, "jpg", baos);
-        baos.flush();
-        byte[] imageData = baos.toByteArray();
-        baos.close();
+    public void sendCompressedBufferedImageOverUDP(BufferedImage img) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(img, "jpg", baos);
+            baos.flush();
+            byte[] imageData = baos.toByteArray();
+            baos.close();
 
-        // Compress the image data
-        ByteArrayOutputStream compressedStream = new ByteArrayOutputStream();
-        try (GZIPOutputStream gzipStream = new GZIPOutputStream(compressedStream)) {
-            gzipStream.write(imageData);
+            // Compress the image data
+            ByteArrayOutputStream compressedStream = new ByteArrayOutputStream();
+            try (GZIPOutputStream gzipStream = new GZIPOutputStream(compressedStream)) {
+                gzipStream.write(imageData);
+            }
+            byte[] compressedImageData = compressedStream.toByteArray();
+
+            DatagramPacket packet = new DatagramPacket(compressedImageData, compressedImageData.length, destinationIP, destinationPort);
+            socket.send(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        byte[] compressedImageData = compressedStream.toByteArray();
-
-        DatagramPacket packet = new DatagramPacket(compressedImageData, compressedImageData.length, destinationIP, destinationPort);
-        socket.send(packet);
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-    }
-    public BufferedImage receiveBufferedImageOverUDP() {
-    byte[] receiveData = new byte[65537];
-    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-    try {
-        socket.receive(receivePacket); // Receive the UDP packet
-        // Convert the received data to a BufferedImage
-        ByteArrayInputStream bais = new ByteArrayInputStream(receivePacket.getData());
-        BufferedImage receivedImage = ImageIO.read(bais);
-        return receivedImage;
-    } catch (Exception ex) {
-        ex.printStackTrace();
-        return null;
-    }
     }
 
+    public BufferedImage receiveCompressedBufferedImageOverUDP() {
+        byte[] receiveData = new byte[65537];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        try {
+            socket.receive(receivePacket); // Receive the UDP packet
+
+            // Store the received data in the circular buffer
+            circularBuffer[nextIndex] = receivePacket.getData();
+            nextIndex = (nextIndex + 1) % 10; // Update the index for the next packet
+
+            // Decompress the received data
+            ByteArrayInputStream bais = new ByteArrayInputStream(receivePacket.getData());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try (GZIPInputStream gzipStream = new GZIPInputStream(bais)) {
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = gzipStream.read(buffer)) != -1) {
+                    baos.write(buffer, 0, len);
+                }
+            }
+            byte[] decompressedData = baos.toByteArray();
+
+            // Convert the decompressed data to a BufferedImage
+            ByteArrayInputStream imageStream = new ByteArrayInputStream(decompressedData);
+            BufferedImage receivedImage = ImageIO.read(imageStream);
+            return receivedImage;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
